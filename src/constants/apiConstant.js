9fc155e53d1a290e6348a7964b92334198061378
Yogesh <yogesh.kandari@imagine.io>
2024-08-07 16:25:14 +0530

    changes

--- src/constants/apiConstant.js
+++ src/constants/apiConstant.js
@@ -2,10 +2,10 @@
 export const API_ROOT_URL = 'https://prod.imagine.io';
 export const API_ROOT_URL_DEV = 'https:dev2.imagine.io';
 
-export const GET_SCENE_LABEL_PUBLIC_API = '/configurator/api/v2/sceneoption-public-data/';
-export const GET_LABEL_OPTIONS_PUBLIC_API = '/configurator/api/v2/scenetexture-public-data/';
+export const GET_SCENE_LABEL_PUBLIC_API = '/configurator/api/v2/sceneoption-public-data/';  //label
+export const GET_LABEL_OPTIONS_PUBLIC_API = '/configurator/api/v2/scenetexture-public-data/'; // texture or finish
 export const GET_PROTOTYPE_PREVIEWS_API = '/configurator/api/v2/update-scene-texture-render/';
-export const GET_SCENE_VIEW_BG_INFO_PUBLIC_API = '/configurator/api/v2/scene-view-background-info-public/';
+export const GET_SCENE_VIEW_BG_INFO_PUBLIC_API = '/configurator/api/v2/scene-view-background-info-public/'; //camera angle id
 
 // render & preview//
 export const GET_RENDER_PREVIEW = '/rendering-system/api/v3/project-render-images/';
@@ -16,9 +16,9 @@ export const RENDER_PARTIAL_PAYMENT = '/rendering-system/api/v3/render-partial-p
 export const GET_RENDER_STORE_LIST = '/configurator/api/v2/get-render-store/';
 export const REORDER_VIEWS = '/configurator/api/v2/multiple-sceneview-seq-update/';
 export const CONFIG_PUBLIC_LINK = 'configurator/api/config-public-link/';
-export const CONFIG_PUBLIC_DATA = 'configurator/api/config-public-data/';
+export const CONFIG_PUBLIC_DATA = 'configurator/api/config-public-data/'; //configurator data
 export const GET_LABEL_OPTIONS_API = '/configurator/api/v2/scene-texture/';
-export const GET_SCENE_PUBLIC_DATA = '/configurator/api/v2/all-scene-public-data/';
+export const GET_SCENE_PUBLIC_DATA = '/configurator/api/v2/all-scene-public-data/';// scene id
 export const CHECK_FOR_UNICODE = '';
 
 //wardrobe api
